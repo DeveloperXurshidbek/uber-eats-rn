00cb6950596e810373a74690d62467c61ccbe4e0
Xurshidbek <developerxurshidbek@gmail.com>
2022-02-12 22:31:09 +0500

    chaging status

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
@@ -7,15 +7,45 @@ import RestaurantItems, {
   localRestaurants,
 } from "../components/RestaurantItems";
 
+const YELP_API_KEY =
+  "GAUq9NvMU6Bq_a2vQyCrz2uyBszeiBReM-_3ktu0vqV__cfLlp9RORoODULBdD_i67APzSBazl0-V2NzhrD8YPopGspD0nm_2Lp76JTIJ5PPQn6fm2gczR6BhNEHYnYx";
+
 export default function Home() {
-  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("Chicago");
+  const [activeTab, setActiveTab] = useState("Delivery");
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city, activeTab]);
+
   return (
     <SafeAreaView
       style={{ paddingVertical: 35, backgroundColor: "#eee", flex: 1 }}
     >
       <View style={{ backgroundColor: "white", padding: 15 }}>
-        <HeaderTabs />
-        <SearchBar />
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar cityHandler={setCity} />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
